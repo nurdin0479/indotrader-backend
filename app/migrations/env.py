7f5d68b4fd6585a8ccd3702a1491703cd3d6c135
nurdin <nurdin0479@gmail.com>
2025-11-19 09:55:36 +0700

    update env

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -1,28 +1,20 @@
 from logging.config import fileConfig
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 from alembic import context
 
-import os
 import sys
+import os
 
-# Tambahkan path project agar bisa import app.*
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Tambahkan path /app
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from app.models import Base  # Base yang benar
-from app.core.config import settings  # untuk DB URL
+# Import Base dari model
+from model import Base
 
-# Ini config Alembic
 config = context.config
 
-# Set URL dari .env (via settings)
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+fileConfig(config.config_file_name)
 
-# Konfigurasi logging
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
-# Target metadata untuk autogenerate
 target_metadata = Base.metadata
 
 
@@ -49,7 +41,7 @@ def run_migrations_online():
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata,
+            target_metadata=target_metadata
         )
 
         with context.begin_transaction():
