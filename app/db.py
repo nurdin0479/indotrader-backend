bd1117eb999c415ad7005995153ef02b519a5791
nurdin <nurdin0479@gmail.com>
2025-11-19 18:48:27 +0700

    rev

--- app/db.py
+++ app/db.py
@@ -1,7 +1,10 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/postgres"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
