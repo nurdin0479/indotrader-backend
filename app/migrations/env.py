3c754d91ea291d9f7d37295e30a621a8e52bd1ad
nurdin <nurdin0479@gmail.com>
2025-11-19 18:50:18 +0700

    rev all

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -1,57 +1,11 @@
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
-import os
-import sys
 
-# Tambahkan path /app agar bisa import model.py
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from model import Base  # IMPORT MODEL ANDA
-
-# Ini harus sama dengan DATABASE_URL di db.py
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/postgres"
+from db import Base  # <-- sesuai struktur /app/db.py
+from model import Admin, User  # pastikan model terdaftar
 
 config = context.config
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
-
-# Interpret konfigurasi logging dari file alembic.ini
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
-
-
-def run_migrations_offline():
-    context.configure(
-        url=DATABASE_URL,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
-def run_migrations_online():
-    connectable = engine_from_config(
-        configuration=config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
-
-
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
